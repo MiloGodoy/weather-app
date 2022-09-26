e4aed4ec5f862d105378bb1c2a6bce69f4c6234d
Ramiro Godoy <milogodoy@hotmail.com>
2022-09-26 17:21:28 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+window.addEventListener('load', () => {
+    let lon;
+    let lat;
+    
+    let temperaturaValor = document.getElementById('temperatura-valor');
+    let temperaturaDescripcion = document.getElementById('temperatura-descripcion');
+
+    let ubicacion = document.getElementById('ubicacion');
+    let iconoAnimado = document.getElementById('icono-animado');
+
+    let vientoVelocidad = document.getElementById('viento-velocidad');
+
+    if(navigator.geolocation){
+
+        navigator.geolocation.getCurrentPosition(posicion => {
+            //console.log(posicion.coords.latitude);
+            lon = posicion.coords.longitude;
+            lat = posicion.coords.latitude;
+
+            //ubicacion actual
+            //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=bd2466c3f8a69a4d18e3459cdaea4175`;
+
+            //ubicacion por ciudad
+            
+
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=Asuncion&lang=es&units=metric&appid=bd2466c3f8a69a4d18e3459cdaea4175`
+            
+            //console.log(url);
+
+            fetch(url)
+                .then( response => { return response.json() })
+                .then (data => {
+                    let temp = Math.round(data.main.temp)
+                    temperaturaValor.textContent = `${temp} `;
+                    console.log(data.weather[0].description);
+                    
+                    let desc = data.weather[0].description
+                    temperaturaDescripcion.textContent = desc.toUpperCase();
+                    console.log(data.name);
+
+                    ubicacion.textContent = data.name;
+                    console.log(data.wind.speed);
+
+                    vientoVelocidad.textContent = `${data.wind.speed} m/s`
+
+                    console.log(data.weather[0].main);
+                    switch(data.weather[0].main) {
+                        case 'Clear':
+                            iconoAnimado.src = './animated/day.svg';
+                            console.log('LIMPIO');
+                            break;
+                        case 'Clouds':
+                            iconoAnimado.src = './animated/cloudy-day-1.svg';
+                            console.log('NUBLADO');
+                            break;
+                        case 'Thunderstorm':
+                            iconoAnimado.src = './animated/thunder.svg';
+                            console.log('Tormenta');
+                            break;
+                        case 'Drizzle':
+                            iconoAnimado.src = './animated/rainy-2.svg';
+                            console.log('Llovizna');
+                            break;
+                        case 'Rain':
+                            iconoAnimado.src = './animated/rainy-7.svg';
+                            console.log('Lluvia');
+                            break;
+                        case 'Snow':
+                            iconoAnimado.src = './animated/snowy-6.svg';
+                            console.log('Nieve');
+                            break;
+                        case 'Atmosphere':
+                            iconoAnimado.src = './animated/weather.svg';
+                            console.log('Atmósfera');
+                            break;
+                        default:
+                            iconoAnimado.src = './animated/cloudy-day-3.svg';
+                            console.log('Por defecto');
+                    }
+
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        })
+    }
+})
---
+++ secrets.js
@@ -0,0 +1,1 @@
+const API_KEY = 'bd2466c3f8a69a4d18e3459cdaea4175';
